7c18dbca97302f21ca3b829d93d742bf05a91dbc
rishabghkr <rishab.ghosh@kroger.com>
2023-05-04 17:03:04 +0530

    run it with es6 syntax.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const {logInformation} = require("./lib/loggerUtils");
-const {setResponseHeaders} = require("./lib/apiUtils");
+import express from 'express';
+import {setResponseHeaders} from "./lib/apiUtils.js";
+import {logInformation} from "./lib/loggerUtils.js";
 const app = express();
 const PORT = 3001;
 
